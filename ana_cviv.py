fd100c4b04a1d9e50fe503f49d74a52d73098316
Vendula Maulerova <maulerova@gmail.com>
2020-12-01 16:27:02 +0100

    vc, iv and cvf in

--- ana_cviv.py
+++ ana_cviv.py
@@ -6,8 +6,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rc
 
-rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
+# rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 rc('text', usetex=True)
+
+
+def dep_voltage(v_det, capacitance, i_det):
+    """finds the depletion voltage, based on
+    http://cds.cern.ch/record/2639974/files/
+    automatic-method-extracting-final-Martin-Petek.pdf
+    Another approach is by Pablo: https://cds.cern.ch/
+    record/2281851/files/PabloMatorras2017.pdf
+    """
+    # Setting Pablo approach as default
+    plot_cv(v_det, capacitance, "test")
+    v_det_new = []
+    try:
+        i = 1
+        while True:
+            k_bd = (i_det[i]-i_det[i-1])/(v_det[i]-v_det[i-1])*(v_det[i]/i_det[i])
+            i += 1
+            if k_bd > 4:
+                print(k_bd)
+                print(i)
+                if i > 20000:
+                    break
+    except NameError:
+        print("The cv measurements are not nice.")
+    return v_det, capacitance
 
 
 def read_iv(filename):
@@ -43,10 +68,10 @@ def read_cvf(filename):
         if data[1][no_of_frequencies] == freq_init:
             break
     for i in range(no_of_frequencies):
-        v_det = data[0][i::no_of_frequencies]
+        v_det = np.abs(data[0][i::no_of_frequencies])
         capacitance = data[2][i::no_of_frequencies]
         sigma = data[3][i::no_of_frequencies]
-        v_bias = data[4][i::no_of_frequencies]
+        v_bias = np.abs(data[4][i::no_of_frequencies])
         i_ps = data[5][i::no_of_frequencies]
         frequency = frequency_object(v_det=v_det, frequency=data[1][i],
                                      c=capacitance, sigma=sigma, v_bias=v_bias,
@@ -55,56 +80,70 @@ def read_cvf(filename):
     return frequency_array
 
 
-def plot_iv(v_b, v_g, i_p):
+def plot_iv(v_b, v_g, i_p, graph_label):
     """Plotting of the IV characteristics
     """
     try:
         plt.figure()
-        plt.plot(v_b, i_p, label="sample_graph_label")
+        plt.plot(v_b, i_p, label=graph_label)
         plt.xlabel(r"$V_B$ [V]")
         plt.ylabel(r"$I_{pad}$ [A]")
-        plt.figure()
-        plt.plot(v_g, i_p)
-        plt.xlabel(r"$V_B$ [V]")
-        plt.ylabel(r"$I_{gate}$ [A]")
+        plt.legend()
+        # plt.figure()
+        # plt.plot(v_g, i_p, label=graph_label)
+        # plt.xlabel(r"$V_B$ [V]")
+        # plt.ylabel(r"$I_{gate}$ [A]")
+        # plt.legend()
     except NameError:
         print("IV measurement not found.")
         plt.close()
 
 
-def plot_cv(v_b, capacitance):
+def plot_cv(v_d, capacitance, graph_label):
     """Plotting of the CV characteristics
     """
     try:
-        plt.figure()
-        plt.plot(v_b, capacitance, label="sample_graph_label")
-        plt.xlabel("$V_B$ [V]")
-        plt.ylabel("Capacitance [A]")
+
+        _fig, (ax1, ax2) = plt.subplots(1, 2)
+        ax1.plot(v_d, c*10**(12),
+                 label=str(graph_label))
+        ax1.set_xlabel("$V_{det}$ [V]")
+        ax1.set_ylabel("C [pF]")
+        ax1.legend()
+
+        ax2.plot(v_d, 1/((capacitance*10**(12))**2),
+                 label=str(graph_label))
+        ax2.set_xlabel("$V_{det}$ [V]")
+        ax2.set_ylabel("1/C$^2$ [pF$^{-1}$)]")
+        ax2.legend()
+
     except NameError:
-        print("CV measurement not found.")
+        print("CV measurements not found.")
         plt.close()
 
 
-def plot_cvf(data_frequency):
+def plot_cvf(data_frequency, graph_label):
     """Plotting of the CVF characteristics
     """
     try:
-        plt.figure()
-        for freq in data_frequency:
-            plt.plot(freq.v_bias, freq.c,
-                     label=freq.frequency)
-        plt.xlabel("$V_{Bias}$ [V]")
-        plt.ylabel("Capacitance [A]")
-        plt.legend()
-        plt.show()
 
-        plt.figure()
+        _fig, (ax1, ax2) = plt.subplots(1, 2)
+        # fig.suptitle('Horizontally stacked subplots')
+        # ax1.plot(x, y)
+        # ax2.plot(x, -y)
         for freq in data_frequency:
-            plt.plot(freq.v_bias, 1/(freq.c**2),
-                     label=freq.frequency)
-        plt.xlabel("$V_{Bias}$ [V]")
-        plt.ylabel("1/C^2 [A]")
-        plt.legend()
+            ax1.plot(freq.v_det, freq.c*10**(12),
+                     label=str(freq.frequency)+" Hz")
+        ax1.set_xlabel("$V_{det}$ [V]")
+        ax1.set_ylabel("C [pF]")
+        ax1.legend()
+
+        for freq in data_frequency:
+            ax2.plot(freq.v_det, 1/((freq.c*10**(12))**2),
+                     label=str(freq.frequency)+" Hz "+str(graph_label))
+        ax2.set_xlabel("$V_{det}$ [V]")
+        ax2.set_ylabel("1/C$^2$ [pF$^{-1}$)]")
+        ax2.legend()
 
     except NameError:
         print("CVF measurements not found.")
@@ -113,25 +152,43 @@ def plot_cvf(data_frequency):
 
 # FOLDER = "data/CIS16-EPI-09-50-DS-54/CIS16-EPI-09-50-DS-54_2020-11-26_1.cv"
 # DATA = "../data/CIS16-EPI-09-50-DS-54/CIS16-EPI-09-50-DS-54_2020-11-26_1.iv"
-DATA = "../data/CIS16-EPI-09-50-DS-54/CIS16-EPI-09-50-DS-54_2020-11-26_1.cvf"
-sample_name = os.path.basename(DATA)[0:21]
-sample_graph_label = sample_name[6:12]  # prints for example EPI-09
+# DATA = "../data/CIS16-EPI-04-100-DS-95/CIS16-EPI-04-100-DS-95_2020-11-30_1.cvf"
+# DATA = "../data/CIS16-EPI-09-50-DS-60/CIS16-EPI-09-50-DS-60_2020-12-01_2.iv"
+DATA = "../data/CIS16-EPI-15-100-DS-61/CIS16-EPI-15-100-DS-61_2020-12-01_1.cvf"
+sample_name = os.path.basename(DATA)[0:-17]
+# sample_graph_label = sample_name[6:12]  # prints for example EPI-09
+sample_graph_label = sample_name[6:]  # prints for example EPI-09
 print(sample_graph_label)
-
+# ###########################IV FILES#######################
+# ##########################################################
 if DATA.endswith("iv"):
     V_bias, I_tot, I_pad = read_iv(DATA)
+    V_bias = np.abs(V_bias)
+    I_pad = np.abs(I_pad)
+    I_tot = np.abs(I_tot)
     V_gate = I_tot-I_pad
     d_I = []
-    for i in range(1, V_bias):
-        d_I.append((I_pad[i]-I_pad[i-1])/(V_bias[i]-V_bias[i-1]))
-    plot_iv(V_bias, V_gate, I_pad)
+    # for j in range(1, V_bias):
+        # d_I.append((I_pad[j]-I_pad[j-1])/(V_bias[j]-V_bias[j-1]))
+    plot_iv(V_bias, V_gate, I_pad, sample_graph_label)
+    plt.savefig(sample_graph_label+"IV.png")
+# ###########################CV FILES#######################
+# ##########################################################
 elif DATA.endswith("cv"):
     V_det, C, Sigma, V_bias, I_ps = read_cv(DATA)
+    V_det = np.abs(V_det)
+    V_bias = np.abs(V_bias)
     d_C = []
-    for i in range(1, V_bias):
-        d_C.append((C[i]-C[i-1])/(V_det[i]-V_det[i-1]))
-    plot_cv(V_bias, C)
+    for j in range(1, V_det):
+        d_C.append((C[j]-C[j-1])/(V_det[j]-V_det[j-1]))
+    plot_cv(V_bias, C, sample_name)
+    plt.savefig(sample_graph_label+"CV.png")
+# ###########################CVF FILES#######################
+# ##########################################################
 elif DATA.endswith("cvf"):
     Data = read_cvf(DATA)
-    plot_cvf(Data)
+    # frequency = Data[1]
+    # dep_voltage(frequency.v_det, frequency.c, frequency.i_ps)
+    plot_cvf(Data, sample_graph_label)
+    plt.savefig(sample_graph_label+"CVF.png")
 plt.show()
