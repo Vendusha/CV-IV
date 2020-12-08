c247adc2533dcafa137f7319a1047803d0b3552c
Vendula Maulerova <maulerova@gmail.com>
2020-12-08 16:40:05 +0100

    Adding IV to the final plot. Dividing the frequencies by colors.

--- ana_cviv.py
+++ ana_cviv.py
@@ -1,7 +1,6 @@
 """This script is provides tools to analyze CVIV measurements
 """
 import os
-import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import rc
 import read_cviv
@@ -17,45 +16,56 @@ def listdir_fullpath(directory):
 rc('text', usetex=True)
 
 
-FOLDER = "../data/CIS16-EPI-09-50-DS-54"
-BASENAME = (os.path.split(FOLDER))[-1]
-if BASENAME[15] == "-":
-    thickness = int(BASENAME[13:15])
-else:
-    thickness = int(BASENAME[13:16])
-FOLDER = listdir_fullpath(FOLDER)
-sample_graph_label = BASENAME[6:]  # prints for example EPI-09
-# ###########################IV FILES#######################
-# ##########################################################
-for DATA in FOLDER:
-    if DATA.endswith("iv"):
-        V_bias, I_tot, I_pad = read_cviv.read_iv(DATA)
-        V_bias = np.abs(V_bias)
-        I_pad = np.abs(I_pad)
-        I_tot = np.abs(I_tot)
-        V_gate = I_tot-I_pad
-        # read_cviv.plot_iv(V_bias, V_gate, I_pad, sample_graph_label)
-        # plt.savefig(sample_graph_label+"IV.png")
-for DATA in FOLDER:
-    # ###########################CV FILES#######################
+def data_ana(sample_folder):
+    """Analyses a folder of a sample generated
+    by the CVIV labview.
+    """
+    print(sample_folder)
+    basename = (os.path.split(sample_folder))[-1]
+    if basename[15] == "-":
+        thickness = int(basename[13:15])
+    else:
+        thickness = int(basename[13:16])
+    sample_folder = listdir_fullpath(sample_folder)
+    sample_graph_label = basename[6:]  # prints for example EPI-09
+    # ###########################IV FILES#######################
     # ##########################################################
-    # elif DATA.endswith("cv"):
-    #     V_det, C, Sigma, V_bias, I_ps = read_cviv.read_cv(DATA)
-    #     V_det = np.abs(V_det)
-    #     V_bias = np.abs(V_bias)
-    #     I_ps = np.abs(I_ps)
-    #     # d_C = []
-    #     # for j in range(1, V_det):
-    #     #     d_C.append((C[j]-C[j-1])/(V_det[j]-V_det[j-1]))
-    #     read_cviv.plot_cv(V_bias, C, sample_name)
-    #     # plt.savefig(sample_graph_label+"CV.png")
-    # ###########################CVF FILES#######################
-    # ##########################################################
-    if DATA.endswith("cvf"):
-        Data = read_cviv.read_cvf(DATA)
-        Frequency, N_eff, V_dep = read_cviv.plot_cvf(Data,
-                                                     sample_graph_label,
-                                                     I_pad, thickness)
-        # plt.savefig(sample_graph_label+"CVF.png")
-plt.close()
-# plt.show()
+    for data in sample_folder:
+        if data.endswith("iv"):
+            v_bias, i_tot, i_pad = read_cviv.read_iv(data)
+            v_bias = np.abs(v_bias)
+            i_pad = np.abs(i_pad)
+            i_tot = np.abs(i_tot)
+            # v_gate = i_tot-i_pad
+            # read_cviv.plot_iv(V_bias, V_gate, I_pad, sample_graph_label)
+            # plt.savefig(sample_graph_label+"IV.png")
+    for data in sample_folder:
+        # ###########################CV FILES#######################
+        # ##########################################################
+        # elif DATA.endswith("cv"):
+        #     V_det, C, Sigma, V_bias, I_ps = read_cviv.read_cv(DATA)
+        #     V_det = np.abs(V_det)
+        #     V_bias = np.abs(V_bias)
+        #     I_ps = np.abs(I_ps)
+        #     # d_C = []
+        #     # for j in range(1, V_det):
+        #     #     d_C.append((C[j]-C[j-1])/(V_det[j]-V_det[j-1]))
+        #     read_cviv.plot_cv(V_bias, C, sample_name)
+        #     # plt.savefig(sample_graph_label+"CV.png")
+        # ###########################CVF FILES#######################
+        # ##########################################################
+        if data.endswith("cvf"):
+            _no_line, date = read_cviv.find_start(data)
+            data_frequency = read_cviv.read_cvf(data)
+            _Frequency, _N_eff, _V_dep = read_cviv.plot_cvf(data_frequency,
+                                                         sample_graph_label,
+                                                         i_pad, thickness,
+                                                            date, v_bias)
+            # plt.savefig(sample_graph_label+"CVF.png")
+    return
+
+
+DIRECTORY = "../data"
+DIRECTORY = [f.path for f in os.scandir(DIRECTORY) if f.is_dir()]
+for folder in DIRECTORY:
+    data_ana(folder)
--- read_cviv.py
+++ read_cviv.py
@@ -2,11 +2,14 @@
 """
 from collections import namedtuple
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
-from matplotlib import rc
-rc('text', usetex=True)
+from scipy import interpolate
+from cycler import cycler
+mpl.rc('text', usetex=True)
+mpl.rcParams['axes.prop_cycle'] = cycler(color='bgrcmyk')
 
 
 def find_start(file_name):
@@ -15,23 +18,26 @@ def find_start(file_name):
     line_no = 0
     with open(file_name, 'r') as read_obj:
         for num, line in enumerate(read_obj):
-            if "BEGIN" in line:
-                line_no = num+1
+            if ":start" in line:
+                datetime = next(read_obj)
+            elif "BEGIN" in line:
+                line_no = num+2
                 break
     try:
-        return line_no
+        return line_no, datetime
     except NameError:
         print("String BEGIN not found in file.")
 
 
-def n_eff_fcn(v_depletion, d_thickness):
+def n_eff_fcn(v_d, d_t):
     """Calculates the effective doping of a silicon sample.
-    Returns the doping in 10**(-7).
+    Returns the doping in 10**(13). Takes arguments
+    depletion voltage and thickness
     """
-    epsilon_0 = 8.8541878128  # 10^{-12} F m^(-1)
-    epsilon_si = 11.68  # relative permitivity
+    eps_0 = 8.8541878128  # 10^{-12} F m^(-1)
+    eps_si = 11.68  # relative permitivity of Silicon
     q_0 = 1.602  # in 10^(-19)
-    return np.abs(2*epsilon_si*epsilon_0**2*v_depletion/(q_0*d_thickness**2))
+    return np.abs(2*eps_si*eps_0*v_d/(q_0*d_t**2))
 
 
 def check_breakdown(v_det, capacitance, i_det):
@@ -46,6 +52,46 @@ def check_breakdown(v_det, capacitance, i_det):
             capacitance = capacitance[:i-1]
             break
     return v_det, capacitance
+
+
+def dep_voltage_curvature(v_det, capacitance):
+    """finds the depletion voltage, based on https:/
+    /stackoverflow.com/questions/29382903
+    /how-to-apply-piecewise-linear-fit-in-python?rq=1
+    second derivation method based on window from the initial guess
+    """
+    capacitance = 1/(capacitance**2)
+    tck = interpolate.splrep(v_det, capacitance, k=2, s=0)
+    dev_1 = interpolate.splev(v_det, tck, der=1)
+    dev_2 = interpolate.splev(v_det, tck, der=2)
+    curvature = np.abs(((1+dev_1**2)**1.5)/dev_2)
+    turning_point_mask = np.amin(curvature)
+    print(turning_point_mask)
+    # turning_point_mask = dev_2 == np.amin(dev_2)
+    xnew = np.linspace(v_det[0], v_det[len(v_det)-1])
+    _fig, axes = plt.subplots(3)
+    axes[0].plot(v_det, capacitance, 'x', label='data')
+    axes[0].plot(xnew, interpolate.splev(xnew, tck, der=0), label='Fit')
+    axes[1].plot(v_det, interpolate.splev(v_det, tck, der=1), label='1st dev')
+    axes[2].plot(v_det, dev_2, label='2st dev')
+    axes[2].plot(v_det[turning_point_mask], dev_2[turning_point_mask],
+                 'rx', label='Turning point')
+    for axis in axes:
+        axis.legend(loc='best')
+    plt.show()
+    return v_det[turning_point_mask]
+
+
+def select_slice_vdep(v_det, capacitance, percentage, v_dep_init):
+    """Selects a percentage slice of an array
+    around a given point, percentage should be given in
+    interval (0,1)
+    """
+    indeces = int(percentage * len(v_det))
+    middle_index = np.searchsorted(v_det, v_dep_init)[0]
+    v_det = v_det[middle_index-indeces:middle_index+indeces]
+    capacitance = capacitance[middle_index-indeces:middle_index+indeces]
+    return (v_det, capacitance)
 
 
 def dep_voltage(v_det, capacitance, i_det):
@@ -74,6 +120,10 @@ def dep_voltage(v_det, capacitance, i_det):
         #        # [ys_sl[msk][0], ys_sl[msk][-1]],
         #        # color='r', zorder=1)
     v_depletion = np.abs((coeff[1]-coeff[3])/(coeff[0]-coeff[2]))
+    # v_det, capacitance = select_slice_vdep(v_det, capacitance,
+    #                                        0.15, v_depletion)
+    # v_depletion = dep_voltage_curvature(v_det, capacitance)
+    # v_depletion = dep_voltage_curvature(v_depletion)
     # read_cviv.plot_cv(v_det, 1/capacitance**2, "test", v_dep)
     # plt.show()
     return v_depletion
@@ -82,7 +132,7 @@ def dep_voltage(v_det, capacitance, i_det):
 def read_iv(filename):
     """read the files IV extension, returns the bias, total current and pad current
     """
-    no_line_start = find_start(filename)
+    no_line_start, _datetime = find_start(filename)
     data = np.genfromtxt(filename, dtype=float, skip_header=no_line_start,
                          skip_footer=1, unpack=True)
     return data
@@ -92,7 +142,7 @@ def read_cv(filename):
     """read the files IV extension, returns the detector Voltage, Capacitance,
     Conductivity, Bias and Current in Power Supply
     """
-    no_line_start = find_start(filename)
+    no_line_start, _datetime = find_start(filename)
     data = np.genfromtxt(filename, dtype=float, skip_header=no_line_start,
                          skip_footer=1, unpack=True)
     return data
@@ -102,7 +152,7 @@ def read_cvf(filename):
     """read the files IV extension, returns the detector Voltage, Capacitance,
     Conductivity, Bias and Current in Power Supply
     """
-    no_line_start = find_start(filename)
+    no_line_start, _datetime = find_start(filename)
     data = np.genfromtxt(filename, dtype=float, skip_header=no_line_start,
                          skip_footer=1, unpack=True)
     frequency_object = namedtuple("Frequency_object",
@@ -166,7 +216,7 @@ def plot_cv(v_d, capacitance, graph_label, v_dep=0):
     axis2.legend()
 
 
-def plot_cvf(data, graph_label, i_pad, thickness):
+def plot_cvf(data, graph_label, i_pad, thickness, date, v_bias):
     """Plotting of the CVF characteristics
     """
     frequency_array = []
@@ -178,17 +228,23 @@ def plot_cvf(data, graph_label, i_pad, thickness):
     if n_total % cols > 0:
         rows += 1
     position = range(1, n_total + 1)
-    fig = plt.figure(figsize=(15, 6))
-    plt.title(label=str(graph_label))
+    fig = plt.figure(figsize=(16, 6))
+    plt.title(str(graph_label))
+    plt.box(False)
+    plt.tick_params(left=False, labelleft=False)
+    plt.tick_params(bottom=False, labelbottom=False)
     for index, frequency in enumerate(data):
+        cmap = plt.get_cmap("tab10")
         v_dep = dep_voltage(frequency.v_det, frequency.c, i_pad)
         # plot_cv(frequency.v_det, frequency.c, graph_label
         # + " "+str(frequency.frequency)+" Hz", v_dep)
         axis = fig.add_subplot(rows, cols, position[index])
         axis.plot(frequency.v_det, 1/(frequency.c**2),
-                  label=str(frequency.frequency)+" Hz")
-        axis.set_xlabel("$V_{det}$ [V]")
+                  label="CV "+str(frequency.frequency)+" Hz", color=cmap(index))
+        # ax2._get_lines.get_next_color()
+        axis.set_xlabel("$V_{bias}$ [V]")
         axis.set_ylabel("1/C$^2$ [F$^{-1}$)]")
+        plt.legend()
         if v_dep != 0:
             axis.axvline(v_dep, ymin=0.8, ymax=0.95,
                          color='r', label="$v_{dep}$")
@@ -196,16 +252,28 @@ def plot_cvf(data, graph_label, i_pad, thickness):
         n_eff.append(n_eff_fcn(v_dep, thickness)[0])
         v_dep_array.append(v_dep[0])
     axis = fig.add_subplot(rows, cols, position[n_total-2])
-    axis.plot(frequency_array, v_dep_array, 'x',
-              label=str(graph_label))
-    axis.set_ylabel("$V_{dep}$ [V]")
-    axis.set_xlabel("Frequency [Hz]")
-    axis = fig.add_subplot(rows, cols, position[n_total-1])
-    axis.plot(frequency_array, n_eff, 'x',
-              label=str(graph_label))
-    axis.set_ylabel("Doping concentration [$10^7$] atoms")
-    axis.set_xlabel("Frequency [Hz]")
+    axis.plot(v_bias, i_pad, label="IV")
+    axis.set_xlabel(r"$V_{bias}$ [V]")
+    axis.set_ylabel(r"$I_{pad}$ [A]")
     plt.legend()
-    plt.savefig(str(graph_label)+"CVF.png")
-    plt.show()
+    axis = fig.add_subplot(rows, cols, position[n_total-1])
+    for index, frequency in enumerate(frequency_array):
+        axis.plot(frequency_array[index], v_dep_array[index], 'x',
+                  label=str(graph_label), color=cmap(index))
+    axis.set_ylabel("$V_{depletion}$ [V]")
+    axis.set_xlabel("Frequency [Hz]")
+    axis.set_xscale("log")
+    axis2 = axis.twinx()
+    for index, frequency in enumerate(frequency_array):
+        axis2.plot(frequency_array[index], n_eff[index], 'x',
+                   label=str(graph_label), color=cmap(index))
+    axis2.set_ylabel("Doping concentration [$10^{13}$ atoms/cm$^3$]")
+
+    # axis.set_xlabel("Frequency [Hz]")
+    # axis.set_xscale("log")
+    # plt.legend()
+    # plt.show()
+    plt.savefig("results/"+str(graph_label)+"-" +
+                date[0:2]+"-"+date[3:5]+"-" +
+                date[6:8]+"CVF.png")
     return frequency_array, n_eff, v_dep
